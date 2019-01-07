ad7af97c50e44cbd6592e3a6ba9e8b4097ab0e47
Uiharu Kazari <xucgphi@gmail.com>
2019-01-07 16:02:11 +0900

    partial update on languages

--- js/lang.js
+++ js/lang.js
@@ -1,4 +1,4 @@
-var langs = ['jp','en', 'cn','fr'];
+var langs = ['ja','en', 'zh','fr'];
 var langCode = sessionStorage.getItem('splang');
 var langJS = null;
 
@@ -11,21 +11,27 @@ var translate = function(jsdata) {
 }
 
 if (langCode==null) {
+  //see here for a list of possible values https://stackoverflow.com/questions/5580876/navigator-language-list-of-all-languages
+  //substr(0,2) get the first two letters. so for example, "fr-BE" and "fr-CA" both become "fr"
 	langCode = navigator.language.substr(0, 2);
 	//alert("langCode is null");
+  console.log("langCode is null");
 }
 
 
 if (langs.includes(langCode)){
   $.getJSON('lang/' + langCode + '.json', translate);
+  console.log("langCode, "+langCode);
 }
 else{
   $.getJSON('lang/en.json', translate);
+  console.log("else");
 }
 
 
 function choose(choice) {
 	sessionStorage.setItem('splang', choice );
 	/*alert("You chossed " + sessionStorage.getItem('splang'));*/
+  console.log("set:",choice);
 	location.reload();
 }
