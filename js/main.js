b21a1699216d38f433d7204bdbe7fd6a51546596
Uiharu Kazari <xucgphi@gmail.com>
2019-01-20 21:39:12 +0900

    start to using parcel

--- js/main.js
+++ js/main.js
@@ -67,57 +67,7 @@ $(document).ready(function() {
     }
   });
 
-  $('.active-testimonial-carousel').owlCarousel({
-    loop: true,
-    dot: true,
-    items: 3,
-    margin: 30,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    animateOut: 'fadeOutLeft',
-    animateIn: 'fadeInRight',
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 3,
-      }
-    }
-  })
-  // -------   Mail Send ajax
-
-  $(document).ready(function() {
-    var form = $('#myForm'); // contact form
-    var submit = $('.submit-btn'); // submit button
-    var alert = $('.alert'); // alert div for show alert message
-
-    // form submit event
-    form.on('submit', function(e) {
-      e.preventDefault(); // prevent default form submit
-
-      $.ajax({
-        url: 'mail.php', // form action url
-        type: 'POST', // form submit method get/post
-        dataType: 'html', // request type html/json/xml
-        data: form.serialize(), // serialize form data
-        beforeSend: function() {
-          alert.fadeOut();
-          submit.html('Sending....'); // change submit button text
-        },
-        success: function(data) {
-          alert.html(data).fadeIn(); // fade in response data
-          form.trigger('reset'); // reset form
-          submit.html(''); // reset submit button text
-        },
-        error: function(e) {
-          console.log(e)
-        }
-      });
-    });
-  });
-
+  
   $(document).ready(function() {
     $('#mc_embed_signup').find('form').ajaxChimp();
   });
